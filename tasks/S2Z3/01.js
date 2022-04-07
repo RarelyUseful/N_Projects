29ad05ea7db1b45309280dfccd3c321afba000ed
Paweł Białobrodzki <bialobrodzki@gmail.com>
2022-04-07 12:42:35 +0200

    S2Z3 catch-up

---
+++ tasks/S2Z3/01.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+//npm install express
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(req.method, req.url, req.query);
+  next();
+};
+
+app.use(loggerMiddleware);
+
+app.get("*", (req, res) => {
+  res.send("hello!");
+});
+
+app.listen(4700, () => console.log("server started"));
