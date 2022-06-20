c9273637f141273609e45bcb8da940a6ec01fd93
Paweł Białobrodzki <bialobrodzki@gmail.com>
2022-06-20 02:15:14 +0200

    user checking middleware added

--- exam2/app.js
+++ exam2/app.js
@@ -30,7 +30,7 @@ x 9. [2 punkty] Usuwanie i modyfikowanie ogłoszeń jest zabezpieczone hasłem (
     przy braku dostępu zwracany jest stosowny komunikat i kod odpowiedzi HTTP. Nie jest wymagane zabezpieczenie na poziomie *produkcyjnym*, raczej podstawowe rozwiązanie.
 10. [4 punkty] Aplikacja ma 3 zdefiniowanych na stałe użytkowników, każdy z nich może usuwać i modyfikować tylko
     te ogłoszenia które sam dodał, przy braku dostępu zwracany jest stosowny komunikat i kod odpowiedzi HTTP
-11. [3 punkty] Aplikacja po uruchomieniu z parametrem (np `node app.js debug`) zapisuje w pliku czas odebrania każdego
+x 11. [3 punkty] Aplikacja po uruchomieniu z parametrem (np `node app.js debug`) zapisuje w pliku czas odebrania każdego
     żądania, metodę HTTP oraz adres na który przyszło żądanie
 x 12. [2 punkty] Aplikacja po odebraniu żądania do adresu który nie istnieje powinna zwracać statyczny obrazek
     zamiast domyślnej strony błędu 404
@@ -38,20 +38,59 @@ x?13. [2 punkty] W przypadku wystąpienia błędów aplikacji, szczegóły błę
     a użytkownik dostaje stosowny komunikat i kod odpowiedzi HTTP */
 // 14. Add sample.env
 
-//const db = require("./dbsetup");
-
 require("dotenv").config();
 const express = require("express");
 const app = express();
 const status = require("http-status");
 const path = require("path");
+const fs = require("fs");
+const argv = require("yargs").argv;
 
-let port = process.env.PORT;
+const port = process.env.PORT;
 const { init, getPosts, getPost, deletePost, addPost, updatePost, replacePost } = require("./dbsetup");
 const filePath404 = path.join(__dirname, "404.jpg");
 
 app.disable("x-powered-by");
 app.use(express.json());
+
+// Task 11 - to run with debugger $ node app.js --debug true
+let debugging = false;
+debugging = argv.debug;
+
+const saveData = (data) => {
+  fs.appendFile(`logs.txt`, data, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+};
+const requestTime = (req, res, next) => {
+  let requestTime = Date.now();
+  saveData("Request time " + requestTime + ", Request URL ." + req.url + ", Method: " + req.method + "\n");
+  next();
+};
+if (debugging) {
+  app.use(requestTime);
+}
+// end of task 11.
+// Task 10. I know it's kind of stupid and this is probably something configurable in database.
+const users = ["Admin", "Chuck Norris", "Queen of England"];
+let currentUser = users[0];
+const userCheckMiddleware = (req, res, next) => {
+  const url = req.url;
+  const [empty, id] = url.split("/posts/");
+  console.log("catched url " + url + " so " + e + " then " + id);
+  const post = getPost(id);
+  postAuthor = post.Author;
+  if (postAuthor == currentUser) {
+    console.log("User authentication: OK");
+    next();
+  } else {
+    console.log("401, Unauthorized.");
+    res.status(401);
+    res.send("user not permitted");
+  }
+};
 
 function isVerifiedToken(token) {
   return token === process.env.TOKEN;
@@ -107,6 +146,9 @@ init()
       authMiddleware(req, res, next);
     });
     app.patch("/posts/:id", async (req, res, next) => {
+      userCheckMiddleware(req, res, next);
+    });
+    app.patch("/posts/:id", async (req, res, next) => {
       const { id } = req.params;
       const modifiedPost = req.body;
 
@@ -133,7 +175,9 @@ init()
     app.delete("/posts/:id", async (req, res, next) => {
       authMiddleware(req, res, next);
     });
-
+    app.delete("/posts/:id", async (req, res, next) => {
+      userCheckMiddleware(req, res, next);
+    });
     app.delete("/posts/:id", async (req, res) => {
       const { id } = req.params;
       const result = await deletePost(id);
